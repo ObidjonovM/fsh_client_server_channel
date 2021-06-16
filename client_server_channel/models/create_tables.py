ab697a9c40742b7fe30809a0ea6bb40bed79ead2
Timur Babadjanov <timur.btb@gmail.com>
2021-06-16 19:26:38 +0500

    added two columns to categories table

--- client_server_channel/models/create_tables.py
+++ client_server_channel/models/create_tables.py
@@ -4,6 +4,42 @@ conn = psycopg2.connect('dbname=timur_dev user=timur_dev password=Fido402')
 
 cur = conn.cursor()
 
+#creating employee_type table
+cur.execute('''
+CREATE TABLE IF NOT EXISTS employee_type (
+	emp_type_id SERIAL PRIMARY KEY,
+	emp_type_name VARCHAR(50) UNIQUE NOT NULL,
+	description VARCHAR(200),
+	date_added TIMESTAMP NOT NULL,
+	date_modified TIMESTAMP NOT NULL);
+''')
+
+
+#creating employees table
+cur.execute('''
+CREATE TABLE IF NOT EXISTS employees (
+	emp_id SERIAL PRIMARY KEY,
+	emp_type_id INT NOT NULL, 
+	first_name VARCHAR(50) NOT NULL,
+	middle_name VARCHAR(50),
+	last_name VARCHAR(50) NOT NULL,
+	birth_date DATE NOT NULL,
+	address_1 VARCHAR(50) NOT NULL,
+	address_2 VARCHAR(50),
+	city VARCHAR(50) NOT NULL,
+	country VARCHAR(50) NOT NULL,
+	zipcode VARCHAR(20) NOT NULL,
+	phone VARCHAR(20) NOT NULL,
+	email VARCHAR(50),
+	last_sign_in TIMESTAMP,
+	date_added TIMESTAMP NOT NULL,
+	date_modified TIMESTAMP NOT NULL,
+
+	FOREIGN KEY(emp_type_id)
+	REFERENCES employee_type(emp_type_id));
+''')
+
+
 # creating category table
 cur.execute('''
 CREATE TABLE IF NOT EXISTS categories (
@@ -13,7 +49,14 @@ CREATE TABLE IF NOT EXISTS categories (
         parent_cat_id INT NOT NULL CHECK(parent_cat_id >= 0),
         leaf_cat BOOLEAN NOT NULL,
         date_added TIMESTAMP NOT NULL,
-        date_modified TIMESTAMP NOT NULL); ''')
+        add_emp_id INT NOT NULL CHECK(add_emp_id > 0),
+	date_modified TIMESTAMP NOT NULL,
+	modify_emp_id INT NOT NULL CHECK(modify_emp_id > 0),
+
+	FOREIGN KEY (add_emp_id)
+	REFERENCES employees(emp_id),
+	FOREIGN KEY (modify_emp_id)
+	REFERENCES employees(emp_id)); ''')
 
 
 #creating product_info table
@@ -84,42 +127,6 @@ CREATE TABLE IF NOT EXISTS clients (
 
 	FOREIGN KEY(subs_id)
 	REFERENCES subscriptions(subs_id));
-''')
-
-
-#creating employee_type table
-cur.execute('''
-CREATE TABLE IF NOT EXISTS employee_type (
-	emp_type_id SERIAL PRIMARY KEY,
-	emp_type_name VARCHAR(50) UNIQUE NOT NULL,
-	description VARCHAR(200),
-	date_added TIMESTAMP NOT NULL,
-	date_modified TIMESTAMP NOT NULL);
-''')
-
-
-#creating employees table
-cur.execute('''
-CREATE TABLE IF NOT EXISTS employees (
-	emp_id SERIAL PRIMARY KEY,
-	emp_type_id INT NOT NULL, 
-	first_name VARCHAR(50) NOT NULL,
-	middle_name VARCHAR(50),
-	last_name VARCHAR(50) NOT NULL,
-	birth_date DATE NOT NULL,
-	address_1 VARCHAR(50) NOT NULL,
-	address_2 VARCHAR(50),
-	city VARCHAR(50) NOT NULL,
-	country VARCHAR(50) NOT NULL,
-	zipcode VARCHAR(20) NOT NULL,
-	phone VARCHAR(20) NOT NULL,
-	email VARCHAR(50),
-	last_sign_in TIMESTAMP,
-	date_added TIMESTAMP NOT NULL,
-	date_modified TIMESTAMP NOT NULL,
-
-	FOREIGN KEY(emp_type_id)
-	REFERENCES employee_type(emp_type_id));
 ''')
 
 
