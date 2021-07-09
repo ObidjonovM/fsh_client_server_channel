c52f6837627f4c94f4d203e8cbfb25e20418383c
Abdulbosit Ergashxo'jayev <hikmatizlab1996@gmail.com>
2021-07-09 18:25:13 +0500

    firmware frontend functionalities

--- client_server_channel/static/js/firmwares/get.js
+++ client_server_channel/static/js/firmwares/get.js
@@ -1,13 +1,39 @@
-function deleteType() {
-    const fw_id = document.getElementById("fw_id")
-    let xhttp = new XMLHttpRequest();
-    xhttp.open('DELETE', '/firmwares/delete/' + fw_id.value, true)
-    xhttp.send()
+const fw_id = document.getElementById("fw_id");
 
-    xhttp.onreadystatechange = () => {
-       if (this.readyState == 4 && this.status == 200) {
-	   console.log(this.responseText);
-       } 
+function deleteFirm() {
+    let xhttp = new XMLHttpRequest();
+
+    var result = confirm("Удалить ?");
+
+    if (result) {
+        xhttp.open('DELETE', '/firmwares/delete/' + fw_id.value, true);
+
+        xhttp.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+
+        xhttp.send();
+
+        xhttp.onreadystatechange = () => {
+
+            if (xhttp.readyState == 4 && xhttp.status == 200) {
+                const resp = JSON.parse(xhttp.responseText);
+                if (resp['success']) {
+                    window.open('/firmwares/get_all', '_self')
+                } else {
+                    alert('Не удалось удалить тип сотрудника!');
+                }
+            }
+
+        }
+
     }
 
+}
+
+
+function updateFirm() {
+    window.open('/firmwares/update/' + fw_id.value, '_self');
+}
+
+function otmenFunction() {
+    window.open('/firmwares/get_all', '_self');
 }
