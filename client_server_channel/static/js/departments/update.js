9f7af1e927d9179341974337a79c15e5358b7aba
Abdulbosit Ergashxo'jayev <hikmatizlab1996@gmail.com>
2021-07-23 11:27:16 +0500

    The deficiencies of the css and html files in the department frontend have been fixed.

--- client_server_channel/static/js/departments/update.js
+++
@@ -1,8 +0,0 @@
-function otmenFunction() {
-
-    const value = location.href.substr(
-        location.href.lastIndexOf('/') + 1
-    );
-
-    window.open('/departments/get/' + value, '_self');
-}
