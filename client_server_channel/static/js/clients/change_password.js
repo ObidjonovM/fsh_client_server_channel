5ec2ac97a07fb244f76d8b1001dec5457b315a2e
webcoder1996 <hikmatizlab1996@gmail.com>
2021-08-10 14:00:25 +0500

    clients change_password frontend functionalities are added

---
+++ client_server_channel/static/js/clients/change_password.js
@@ -0,0 +1,78 @@
+document.getElementById('error_username').style.display = "none";
+
+function validateForm() {
+
+
+    var username = document.getElementById('username').value;
+
+    var password = document.getElementById('password').value;
+
+    var new_password = document.getElementById('new_password').value;
+
+    var new_confirm_password = document.getElementById('new_confirm_password').value;
+
+    if (username == "") {
+
+        document.getElementById('error_username').innerHTML = "**Fill the first name!";
+        document.getElementById('error_username').style.display = "block";
+
+        return false;
+    }
+
+
+    if (!isNaN(username)) {
+        document.getElementById('error_username').innerHTML = "**Faqat . va harf qatnashish kerak!";
+        document.getElementById('error_username').style.display = "block";
+
+
+        return false;
+    }
+
+    if (password == "") {
+
+        document.getElementById('error_password').innerHTML = "**Fill the password please!";
+        document.getElementById('error_password').style.display = "block";
+
+
+        return false;
+    }
+
+
+    if (new_password == "") {
+
+        document.getElementById('error_new_password').innerHTML = "**Fill the new password please!";
+        document.getElementById('error_new_password').style.display = "block";
+
+
+        return false;
+    }
+
+
+    if (new_confirm_password == "") {
+
+        document.getElementById('error_new_confirm_password').innerHTML = "**Fill the new confirm password please!"
+        document.getElementById('error_new_confirm_password').style.display = "block";
+        return false;
+    }
+
+
+    if (new_password != new_confirm_password) {
+
+        var myForm = document.getElementById('myForm');
+        myForm.addEventListener('submit', function (event) {
+            event.preventDefault();
+
+        });
+        alert("Parol bir xil emas!");
+        return false;
+
+    } else {
+        alert("Tabriklaymiz parol o'zgardi!")
+    }
+
+
+};
+
+
+
+
