fef38ce78ebd552fdfffaeae5d25a4a376e768f0
webcoder1996 <hikmatizlab1996@gmail.com>
2021-08-12 18:46:37 +0500

    In update.js, the otmentFunction has been changed and the inputs that remain None have been corrected

--- client_server_channel/static/js/clients/update.js
+++ client_server_channel/static/js/clients/update.js
@@ -1,8 +1,12 @@
 function otmenFunction() {
+    window.open('/clients/account', '_self');
+}
 
-    const value = location.href.substr(
-        location.href.lastIndexOf('/') + 1
-    );
 
-    window.open('/clients/get/' + value, '_self');
+const allInputs = document.querySelectorAll('input')
+
+for(let i=0; i<allInputs.length; i++){
+    if (allInputs[i].defaultValue == 'None') {
+        allInputs[i].defaultValue = '';
+    }
 }
