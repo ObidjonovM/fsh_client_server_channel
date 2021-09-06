fbe7045fbab23ec1e425b344da1faa404b074806
webcoder1996 <hikmatizlab1996@gmail.com>
2021-09-06 10:00:24 +0500

    category menus have been completed

--- client_server_channel/static/js/core/index.js
+++ client_server_channel/static/js/core/index.js
@@ -7,18 +7,9 @@ function SignIn() {
 // sign in close model
 
 
+
+
 // Acardion js and image open
-const link = document.querySelectorAll('.link');
-const submenu = document.querySelectorAll('.submenu');
-const icon = document.querySelectorAll('.fa-chevron-down');
-for (let i = 0; i < link.length; i++) {
-
-    link[i].onclick = function () {
-        submenu[i].classList.toggle('active');
-        icon[i].classList.toggle('active');
-    }
-
-}
 
 
 const product_img = document.getElementById('product_img')
@@ -35,33 +26,40 @@ function get_sub_cat(e) {
             if (resp['data']['category_id'] != undefined) {
                 if (e.nextSibling == null || e.nextElementSibling == null) {
                     let ul = document.createElement('UL');
+                    ul.setAttribute('class', 'submenu');
+
                     for (let i = 0; i < resp['data']['category_id'].length; i++) {
                         var li = document.createElement('LI');
                         let a = document.createElement('A');
-                        a.setAttribute('onclick', 'get_sub_cat(this)');
+                        a.setAttribute('onmousedown', 'get_sub_cat(this)');
+                        a.setAttribute('onmouseup', 'open_sub_cat(this)');
+                        a.setAttribute('class', 'link');
                         a.setAttribute('id', resp['data']['category_id'][i]);
                         let textnode = document.createTextNode(resp['data']['name'][i]);
                         a.appendChild(textnode);
                         li.appendChild(a);
                         ul.appendChild(li);
                     }
-                    e.parentNode.appendChild(ul)
+                    e.parentNode.appendChild(ul);
                 }
             } else {
+
                 xhttp.open('POST', '/products/info/' + e.id, true)
                 xhttp.setRequestHeader("Content-type", "application/json; charset=UTF-8");
                 xhttp.send();
                 xhttp.onreadystatechange = function () {
                     if (this.readyState == 4 && this.status == 200) {
-                        product_img.innerHTML = ''
+                        product_img.innerHTML = '';
                         let resp = JSON.parse(this.responseText);
                         if (resp['data']['product_id'] != undefined) {
                             for (let j = 0; j < resp['data']['product_id'].length; j++) {
+                                let div = document.createElement('DIV');
                                 let img = document.createElement('img');
                                 img.setAttribute('id', resp['data']['product_id'][j]);
                                 img.setAttribute('src', resp['data']['photo'][j]);
                                 img.setAttribute('width', '20%');
-                                product_img.appendChild(img)
+                                div.appendChild(img);
+                                product_img.appendChild(div);
                             }
                         }
                     }
@@ -69,6 +67,50 @@ function get_sub_cat(e) {
             }
         }
     }
+
+
+
+}
+
+
+// function open_sub_cat(e) {
+//     if (e.nextElementSibling.style.maxHeight === '300px'){
+//         e.style.color = '#4D4D4D';
+//         e.nextElementSibling.style.maxHeight = '0px';
+//         e.nextElementSibling.style.opacity = '0';
+//         e.nextElementSibling.style.transaction = '2s ease';
+//
+//     }else {
+//         e.nextElementSibling.style.maxHeight = '300px';
+//         e.nextElementSibling.style.opacity = '1';
+//         e.nextElementSibling.style.transaction = '2s ease';
+//         e.style.color = 'black';
+//     }
+//     e.classList.toggle('active');
+// }
+
+
+
+
+
+
+function open_sub_cat(e) {
+    if (e.nextElementSibling.style.display === 'block'){
+        e.style.color = '#4D4D4D';
+        e.nextElementSibling.style.display = 'none';
+        e.nextElementSibling.style.transaction = '.9s ease';
+
+    }else {
+        e.nextElementSibling.style.display = 'block';
+        e.nextElementSibling.style.transaction = '.9s ease';
+        e.style.color = 'black';
+    }
+    e.classList.toggle('active');
+
+    // if (e.nextElementSibling){
+    //      let i = e.nextElementSibling.children.document.createElement('I');
+    //     i.setAttribute('class', 'fa fa-chevron-down');
+    // }
 }
 
 // Acardion and image js close
