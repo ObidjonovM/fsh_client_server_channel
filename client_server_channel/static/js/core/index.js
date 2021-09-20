75f9c7f79215dc03082ea0b4c5b817530923c0e9
webcoder1996 <hikmatizlab1996@gmail.com>
2021-09-20 18:53:50 +0500

    products were drawn to the index

--- client_server_channel/static/js/core/index.js
+++ client_server_channel/static/js/core/index.js
@@ -1,16 +1,16 @@
 // sign in open model
-
 function SignIn() {
     window.open('/clients/login', '_self');
 }
-
 // sign in close model
 
 
+function addProduct() {
+    window.open('/clients/my_products', '_self');
+}
 
 
 // Acardion js and image open
-
 
 const product_img = document.getElementById('product_img')
 var xhttp = new XMLHttpRequest();
@@ -73,13 +73,6 @@ function get_sub_cat(e) {
 }
 
 
-
-
-
-
-
-
-
 function open_sub_cat(e) {
     if (e.nextElementSibling.style.display === 'block'){
         e.style.color = '#4D4D4D';
@@ -102,44 +95,44 @@ function open_sub_cat(e) {
 // Acardion and image js close
 
 
-
-/***** modal open  *****/
-
-'use strict';
-
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnCloseModal2 = document.querySelector('.close-modal2');
-const btnOpenModal = document.querySelector('.show-modal');
-
-const openModal = function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-};
-
-const closeModal = function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-};
-
-const closeModal2 = function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-};
-// for (let i = 0; i < btnsOpenModal.length; i++)
-    btnOpenModal.addEventListener('click', openModal);
-
-btnCloseModal.addEventListener('click', closeModal);
-btnCloseModal2.addEventListener('click', closeModal2);
-// overlay.addEventListener('click', closeModal);
-
-document.addEventListener('keydown', function (e) {
-    // console.log(e.key);
-
-    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-        closeModal();
-    }
-});
-
-/***** modal close  *****/
+//
+// /***** modal open  *****/
+//
+// 'use strict';
+//
+// const modal = document.querySelector('.modal');
+// const overlay = document.querySelector('.overlay');
+// const btnCloseModal = document.querySelector('.close-modal');
+// const btnCloseModal2 = document.querySelector('.close-modal2');
+// const btnOpenModal = document.querySelector('.show-modal');
+//
+// const openModal = function () {
+//     modal.classList.remove('hidden');
+//     overlay.classList.remove('hidden');
+// };
+//
+// const closeModal = function () {
+//     modal.classList.add('hidden');
+//     overlay.classList.add('hidden');
+// };
+//
+// const closeModal2 = function () {
+//     modal.classList.add('hidden');
+//     overlay.classList.add('hidden');
+// };
+// // for (let i = 0; i < btnsOpenModal.length; i++)
+//     btnOpenModal.addEventListener('click', openModal);
+//
+// btnCloseModal.addEventListener('click', closeModal);
+// btnCloseModal2.addEventListener('click', closeModal2);
+// // overlay.addEventListener('click', closeModal);
+//
+// document.addEventListener('keydown', function (e) {
+//     // console.log(e.key);
+//
+//     if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+//         closeModal();
+//     }
+// });
+//
+// /***** modal close  *****/
