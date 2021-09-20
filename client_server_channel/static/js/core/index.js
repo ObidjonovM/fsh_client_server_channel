8957ae4e22151c6856803ec0ae67fab7868e0e64
webcoder1996 <hikmatizlab1996@gmail.com>
2021-09-20 11:10:27 +0500

    search removed from base. modal and ad_product were added to the index and designi yes was added.

--- client_server_channel/static/js/core/index.js
+++ client_server_channel/static/js/core/index.js
@@ -73,21 +73,7 @@ function get_sub_cat(e) {
 }
 
 
-// function open_sub_cat(e) {
-//     if (e.nextElementSibling.style.maxHeight === '300px'){
-//         e.style.color = '#4D4D4D';
-//         e.nextElementSibling.style.maxHeight = '0px';
-//         e.nextElementSibling.style.opacity = '0';
-//         e.nextElementSibling.style.transaction = '2s ease';
-//
-//     }else {
-//         e.nextElementSibling.style.maxHeight = '300px';
-//         e.nextElementSibling.style.opacity = '1';
-//         e.nextElementSibling.style.transaction = '2s ease';
-//         e.style.color = 'black';
-//     }
-//     e.classList.toggle('active');
-// }
+
 
 
 
@@ -114,3 +100,46 @@ function open_sub_cat(e) {
 }
 
 // Acardion and image js close
+
+
+
+/***** modal open  *****/
+
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnCloseModal2 = document.querySelector('.close-modal2');
+const btnOpenModal = document.querySelector('.show-modal');
+
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+const closeModal2 = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+// for (let i = 0; i < btnsOpenModal.length; i++)
+    btnOpenModal.addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+btnCloseModal2.addEventListener('click', closeModal2);
+// overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+    // console.log(e.key);
+
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
+
+/***** modal close  *****/
