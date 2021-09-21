d7374cf49333b17342bda6cf864cdb02f628ccc7
webcoder1996 <hikmatizlab1996@gmail.com>
2021-09-21 15:53:38 +0500

    The Account() and Products() functions have been added to the Account

--- client_server_channel/static/js/clients/account.js
+++ client_server_channel/static/js/clients/account.js
@@ -1,30 +1,27 @@
-// account id open
-const account = document.getElementById('account');
-
-account.addEventListener('click', () => {
-    window.open('/clients/update', '_self');
-})
-// account id close
-
-
-const products = document.getElementById('products');
-const close = document.getElementById('close');
-const closeBtn = document.getElementById('closeBtn');
-
-products.addEventListener('click', () => {
-    window.open('/clients/my_products', '_self');
-})
-
-close.addEventListener('click', () => {
-    document.getElementById('modal-products').style.display = "none";
-
-})
-
-closeBtn.addEventListener('click', () => {
-    document.getElementById('modal-products').style.display = "none";
-
-})
-
+// client logout open
 function Logout() {
     window.open('/clients/logout', '_self');
 }
+// client logout close
+
+
+// account open
+    function Account() {
+        window.open('/clients/account', '_self');
+    }
+// account close
+
+
+//products open
+    function Products() {
+        window.open('/clients/my_products', '_self');
+    }
+//products close
+
+
+//accountlink1 open
+    function accountLink1() {
+        window.open('/clients/update', '_self');
+    }
+//accountlink1 close
+
