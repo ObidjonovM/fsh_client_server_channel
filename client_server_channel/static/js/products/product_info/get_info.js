61bdc4b2070fae4066cd8c85d701dbf821137d50
webcoder1996 <hikmatizlab1996@gmail.com>
2021-10-05 13:25:52 +0500

    library was added and images were drawn at get_info

--- client_server_channel/static/js/products/product_info/get_info.js
+++ client_server_channel/static/js/products/product_info/get_info.js
@@ -1,29 +1,42 @@
 const sm_photo_li = document.querySelectorAll('.sm-photo-li');
 const org_photo_div = document.querySelectorAll('.org-photo-div');
 
-
+const small_text = document.getElementById('small_text');
+const large_text = document.getElementById('large_text');
 
 function smPhotoLi(x){
-    x.style.border = '1px solid #1b232e';
+    small_text.style.display = 'block';
+    large_text.style.display = 'none';
 }
 
-for (let i=0; i<sm_photo_li.length; i++){
+function smallImgClick(evt) {
 
-    sm_photo_li[i].addEventListener('click', function (){
-        if (org_photo_div[i].style.display == 'block'){
-            console.log(org_photo_div[i]);
-            org_photo_div[i].classList.add('active1');
+        // e.target.classList.toggle('active1');
+    // for (let i=0; i<org_photo_div.length; i++){
+
+        if (evt.style.border == '1px solid #adaeb19e'){
+            // evt.style.border = '1px solid #1b232e';
+            evt.style.border = '1px solid #1b232e';
         }else {
-            console.log(org_photo_div[i]);
-            org_photo_div[i].classList.remove('active1');
+            evt.style.border = '1px solid #adaeb19e';
+            // evt.style.border = '1px solid #adaeb19e';
+            // evt.nextElementSibling.style.border = "1px solid #adaeb19e";
+            // evt.previousElementSibling.style.border = "1px solid #adaeb19e";
         }
+    // }
 
 
 
-    })
+    console.log("1")
+
 }
 
+function largePhotoHoverBlock(){
+    small_text.style.display = 'none';
+    large_text.style.display = 'block';
+}
 
-function displayBlock(e){
-    const block = document.createElement('P');
+function largePhotoHoverNone(){
+    small_text.style.display = 'block';
+    large_text.style.display = 'none';
 }
