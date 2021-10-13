9532630dfd999158c59549e3e0f5d40d3280a3e8
webcoder1996 <hikmatizlab1996@gmail.com>
2021-10-13 12:16:27 +0500

    price has been added to get_info

--- client_server_channel/static/js/products/product_info/get_info.js
+++ client_server_channel/static/js/products/product_info/get_info.js
@@ -4,7 +4,8 @@ const org_photo_div = document.querySelectorAll('.org-photo-div');
 for (let i = 0; i < sm_photo_li.length; i++) {
 
     sm_photo_li[i].addEventListener('click', function () {
-
+        console.log(org_photo_div[i]);
+        sm_photo_li[i].classList.toggle('show2');
         org_photo_div[i].classList.toggle('show');
         console.log('1')
 
