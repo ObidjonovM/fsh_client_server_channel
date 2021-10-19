e54c523356c2ffc0401812b4eb7c9f723f5440f8
webcoder1996 <hikmatizlab1996@gmail.com>
2021-10-19 11:26:16 +0500

    was changed from the employee login to change_password

--- client_server_channel/static/js/employees/login.js
+++ client_server_channel/static/js/employees/login.js
@@ -1,1 +1,5 @@
-   
+let change_password = document.getElementById('change_password');
+
+change_password.addEventListener('click', function () {
+    window.open('/employees/change_password', '_self');
+})
