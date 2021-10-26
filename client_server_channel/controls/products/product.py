be0b4641c58faa2c1e0513f3fcd75bafdd78fb75
Muhammadsodiq Obidjonov <7727047@gmail.com>
2021-10-26 11:54:03 +0500

    The vibration_sensor prefix has been added to ProductC

--- client_server_channel/controls/products/product.py
+++ client_server_channel/controls/products/product.py
@@ -36,6 +36,8 @@ class ProductC:
                 product_info['prefix'] = 'gerkon'
             if str(product_info['product_id']) == '5':
                 product_info['prefix'] = 'water_sensor'
+            if str(product_info['product_id']) == '6':
+                product_info['prefix'] = 'vibration_sensor'
             product_info['date_added'] = now
             product_info['date_modified'] = now
             add_result = ProductTable.insert(product_info)
