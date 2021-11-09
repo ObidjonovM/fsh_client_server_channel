24ed995ab3d9a79c983ab4336e36beda2efe7a64
Abdulbosit Ergashxo'jayev <hikmatizlab1996@gmail.com>
2021-11-09 16:42:52 +0500

    my_products va my_product ga sezgir qo'shildi. s_worker.js o'zgartirildi.

--- client_server_channel/static/js/clients/s_worker.js
+++ client_server_channel/static/js/clients/s_worker.js
@@ -13,11 +13,21 @@ function timedCount() {
         xhttp.open('Post', '/clients/my_products/my_product/get_current_state', true);
         xhttp.setRequestHeader("Content-type", "application/json; charset=UTF-8");
         xhttp.send(JSON.stringify(json));
-        // console.log(json)
+
         xhttp.onreadystatechange = function (ev) {
+
             if (xhttp.readyState == 4 && xhttp.status == 200) {
                 const resp1 = JSON.parse(xhttp.responseText);
-                postMessage(resp1);
+               try {
+                   postMessage(resp1);
+               }
+               catch (e) {
+                   console.log("Error: ", e);
+                   if (e.name == 'InvalidStateError') {
+                       window.location.reload();
+                   }
+               }
+
             }
         }
     }
